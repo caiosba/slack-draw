c567ea35645aec8ffc29e072c186859b91571fec
Caio SBA <caiosba@gmail.com>
2016-04-25 00:44:34 -0300

    Issue #1: CSS fixes

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./crxmake.sh src ~/.ssh/key-chrome-extension-2.pem extension/slackdraw.js.crx version.txt
+./crxmake.sh src ~/.ssh/key-chrome-extension.pem extension/slackdraw.js.crx version.txt
